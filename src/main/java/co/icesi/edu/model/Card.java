d7616d3e325c5c6be9b2bcad75dcd8774d1ba2c8
LunaKtalina <lunamartinez5619@gmail.com>
2024-03-27 08:40:56 -0500

    Stack & Card

--- src/main/java/co/icesi/edu/model/Card.java
+++ src/main/java/co/icesi/edu/model/Card.java
@@ -1,4 +1,33 @@
 package co.icesi.edu.model;
 
 public class Card {
+    public enum Color {
+        BLUE, GREEN, RED, YELLOW, NONE
+    }
+
+    public enum SpecialType {
+        DRAW_TWO, REVERSE, SKIP, WILD, WILD_DRAW_FOUR, NONE
+    }
+
+    private Color color;
+    private int number;
+    private SpecialType specialType;
+
+    public Card(Color color, int number, SpecialType specialType) {
+        this.color = color;
+        this.number = number;
+        this.specialType = specialType;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public SpecialType getSpecialType() {
+        return specialType;
+    }
 }
--- src/main/java/co/icesi/edu/structures/Stack.java
+++ src/main/java/co/icesi/edu/structures/Stack.java
@@ -1,4 +1,49 @@
 package co.icesi.edu.structures;
 
-public class Stack {
+public class Stack<T> {
+    private Node<T> top; // Nodo superior de la pila
+    private int size; // Tamaño de la pila
+
+    // Constructor para inicializar la pila
+    public Stack() {
+        top = null;
+        size = 0;
+    }
+
+    // Método para agregar un elemento a la pila (push)
+    public void push(T element) {
+        Node<T> newNode = new Node<>(element);
+        newNode.setNext(top);
+        top = newNode;
+        size++;
+    }
+
+    // Método para remover y devolver el elemento superior de la pila (pop)
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("La pila está vacía");
+        }
+        T data = top.getElement();
+        top = top.getNext();
+        size--;
+        return data;
+    }
+
+    // Método para obtener el elemento superior de la pila sin removerlo (peek)
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("La pila está vacía");
+        }
+        return top.getElement();
+    }
+
+    // Método para verificar si la pila está vacía
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    // Método para obtener el tamaño de la pila
+    public int size() {
+        return size;
+    }
 }
