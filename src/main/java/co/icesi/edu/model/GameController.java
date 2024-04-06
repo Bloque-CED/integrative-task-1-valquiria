3aa7eae1902ba8001c4a1ccba08cb70f813c5748
Renzo Fernando Mosquera Daza <134429193+RenzoFernando@users.noreply.github.com>
2024-04-06 13:29:28 -0500

    functionalities pt1.3

--- src/main/java/co/icesi/edu/model/GameController.java
+++ src/main/java/co/icesi/edu/model/GameController.java
@@ -11,10 +11,13 @@ public class GameController {
     private PriorityQueue<Player> playerQueue;
     private boolean gameOver;
 
+    private boolean activespecialcard;
+
     public GameController() throws Exception {
         deck = new Deck();
         playerQueue = new PriorityQueue<>();
         gameOver = false;
+        activespecialcard = false;
     }
 
     public void startGame(List<String> playerNames) {
@@ -22,17 +25,17 @@ public class GameController {
         List<Player> players = new ArrayList<>();
 
         // Crear objetos Player y añadirlos a la cola de prioridad y a la lista de jugadores
-        int priority = 1;
+        int priority = playerNames.size();
         for (String name : playerNames) {
             Player player = new Player(name);
-            playerQueue.enqueue(player, priority);
             players.add(player);
-            priority++;
+            playerQueue.enqueue(player, priority);
+            priority--;
         }
 
         // Preparar las cartas para repartir
         Queue<String> cardsToDeal = new Queue<>();
-        int totalCardsToDeal = players.size() * 7;
+        int totalCardsToDeal = players.size() * 2;
         for (int i = 0; i < totalCardsToDeal; i++) {
             String cardId = deck.getDiscardDeck().pop();
             cardsToDeal.enqueue(cardId);
@@ -96,6 +99,21 @@ public class GameController {
         return result;
     }
 
+    public boolean isActivespecialcard() {
+        String topCardId = deck.getPlayDeck().peek();
+        Card topCard = deck.getCardTable().get(topCardId);
+
+        boolean flag = false;
+
+        if (topCard.getSpecialType() == Card.SpecialType.DRAW_TWO || topCard.getSpecialType() == Card.SpecialType.SKIP) {
+            if (activespecialcard == true) {
+                flag = true;
+            } else {
+                flag = false;
+            }
+        }
+        return flag;
+    }
 
     //---------------------------------------------------------------------------------------
 
@@ -104,7 +122,6 @@ public class GameController {
         boolean flag = false;
 
         Player currentPlayer = playerQueue.peek();
-        System.out.println("YOOOOOOOOOOO"+ currentPlayer());
         String cardId = currentPlayer.getHand().get(cardIndex);
         Card playedCard = deck.getCardTable().get(cardId);
 
@@ -121,45 +138,69 @@ public class GameController {
 
                 // Verifica si el juego ha terminado
                 gameOver = checkGameOver();
+                flag = true;
             }
-            flag = true;
-            nextTurn();
+        } else if (topCard.getSpecialType() != Card.SpecialType.CHANGE && topCard.getSpecialType() != Card.SpecialType.REVERSE) {
+            if(playedCard.getColor() == topCard.getColor() ) {
+                // Mueve la carta al montón de descarte
+                currentPlayer.removeCardFromHand(cardId);
+                deck.getPlayDeck().push(cardId);
+
+                // Verifica si el juego ha terminado
+                gameOver = checkGameOver();
+                flag = true;
+                activespecialcard = true;
+            }
+        } else {
+            flag = false;
         }
         return flag;
     }
 
     // Método para manejar el efecto de las cartas especiales
-    private void handleSpecialCardEffect(Card card) {
+    public void handleSpecialCardEffect() {
+        // Obtiene la carta en la cima de la pila de juego para comparar
+        String topCardId = deck.getPlayDeck().peek();
+        Card topCard = deck.getCardTable().get(topCardId);
+
         // Implementación de efectos basada en el tipo de carta especial
-        switch (card.getSpecialType()) {
+        switch (topCard.getSpecialType()) {
             case DRAW_TWO:
                 drawCard(2);
                 nextTurn();
                 break;
-            case REVERSE:
-                // Implementación específica
-                break;
             case SKIP:
                 nextTurn();
                 break;
-            case CHANGE:
-                // Implementación específica
-                break;
         }
+        activespecialcard = false;
     }
 
     // Método para pasar al siguiente turno
     public void nextTurn() {
+
+
+        int size = playerQueue.size();
+
+        List<Player> players = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            Player nextPlayer = playerQueue.dequeue();
+            players.add(nextPlayer);
+        }
+
+        for (Player nextPlayer : players) {
+            playerQueue.enqueue(nextPlayer, 1+size);
+        }
+
         // Extrae al jugador actual de la cola de prioridad.
         Player currentPlayer = playerQueue.dequeue();
 
-        // Calcula la nueva prioridad. Podría ser el tamaño actual de la cola + 1,
-        // asegurando que el jugador vuelve al final de la cola.
-        int newPriority = playerQueue.size() + 1;
-
-        // Vuelve a encolar al jugador con su nueva prioridad, colocándolo al final de la cola.
-        playerQueue.enqueue(currentPlayer, newPriority);
+        // Vuelve a encolar al jugador actual con prioridad 1, colocándolo al final de la cola.
+        playerQueue.enqueue(currentPlayer, 1);
     }
+
+
 
 
     // Método para robar una carta del mazo
@@ -170,7 +211,6 @@ public class GameController {
             String drawnCardId = deck.getDiscardDeck().pop();
             currentPlayer.addCardToHand(drawnCardId);
         }
-        nextTurn();
     }
 
     // Método para verificar el fin del juego
@@ -182,185 +222,11 @@ public class GameController {
         if (currentPlayer.getHand().isEmpty()) {
             // Si la mano del jugador está vacía, el jugador ha ganado
             gameOver = true;
-        }
-
-        return false; // El juego no ha terminado aún
-    }
-
-}
-
-
-/*package co.icesi.edu.model;
-
-import co.icesi.edu.structures.HashTable;
-import co.icesi.edu.structures.PriorityQueue;
-import co.icesi.edu.structures.Queue;
-import co.icesi.edu.structures.Stack;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class GameController {
-    private Deck deck;
-    private List<Player> players;
-    private Queue<Player> turnOrder;
-
-
-    private Card currentCard;
-    private boolean isReverse = false;
-
-    public GameController(List<String> playersNames, int numPlayers) {
-        deck = new Deck(cardReferenceTable);
-        players = new ArrayList<>();
-        for (String name : playersNames) {
-            players.add(new Player(name, cardReferenceTable));
-        }
-        turnOrder = new Queue<>();
-        distributeCards(numCards);
-        initializeGame();
-    }
-
-    private void distributeCards(int numCards) {
-        for (Player player : players) {
-            for (int i = 0; i < numCards; i++) {
-                player.addCardToHand(deck.drawCard());
-            }
-        }
-        Card initialCard = deck.drawCard();
-        deck.discardCard(initialCard);
-
-
-        currentCard = initialCard;
-    }
-
-
-    private void initializeGame() {
-        Card initialCard;
-        do {
-            initialCard = deck.drawCard();
-        } while (initialCard.getSpecialType() != Card.SpecialType.NONE);
-
-        deck.discardCard(initialCard);
-    }
-
-    public boolean playCard(Player player, int cardIndex, Card.Color chosenColor) {
-        if (cardIndex < 0 || cardIndex >= player.getHandSize()) {
-            System.out.println("Índice de carta inválido.");
-            return false;
-        }
-
-        Card cardToPlay = player.getHand().get(cardIndex);
-        if (!isPlayable(cardToPlay)) {
-            System.out.println("La carta seleccionada no se puede jugar.");
-            return false;
-        }
-
-        applyCardEffect(cardToPlay, chosenColor);
-
-        player.removeCardFromHand(cardToPlay);
-        deck.discardCard(cardToPlay);
-
-        if (player.getHandSize() == 0) {
-            System.out.println(player.getName() + " ha ganado el juego!");
             return true;
-        }
-
-        updateTurnOrder();
-        return true;
-    }
-
-    private boolean isPlayable(Card card) {
-        return card.getColor() == currentCard.getColor()  || card.getSpecialType() != currentCard.getSpecialType() || card.getNumber() == currentCard.getNumber();
-    }
-    private void applyCardEffect(Card card, Card.Color chosenColor) {
-        switch (card.getSpecialType()) {
-            case DRAW_TWO:
-                Player nextPlayer = getNextPlayer();
-                drawCards(nextPlayer, 2);
-                break;
-            case REVERSE:
-                isReverse = !isReverse;
-                break;
-            case SKIP:
-                getNextPlayer();
-                break;
-            case CHANGE:
-                //currentCard.setColor();
-                break;
-        }
-    }
-
-    private void drawCards(Player player, int count) {
-        for (int i = 0; i < count; i++) {
-            player.addCardToHand(deck.drawCard());
-        }
-    }
-
-
-    private void updateTurnOrder() {
-        if (!isReverse) {
-            Player player = turnOrder.dequeue();
-            turnOrder.enqueue(player);
         } else {
-            Queue<Player> tempQueue = new Queue<>();
-            while (!turnOrder.isEmpty()) {
-                tempQueue.enqueue(turnOrder.dequeue());
-            }
-            while (!tempQueue.isEmpty()) {
-                turnOrder.enqueue(tempQueue.dequeue());
-            }
-        }
-    }
-
-    private Player getNextPlayer() {
-        if (isReverse) {
-            Queue<Player> tempQueue = new Queue<>();
-            while (!turnOrder.isEmpty()) {
-                tempQueue.enqueue(turnOrder.dequeue());
-            }
-            Player player = tempQueue.dequeue();
-            while (!tempQueue.isEmpty()) {
-                turnOrder.enqueue(tempQueue.dequeue());
-            }
-            return player;
-        } else {
-            return turnOrder.peek();
-        }
-    }
-
-    public boolean isGameOver() {
-        for (Player player : players) {
-            if (player.getHandSize() == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public List<Player> getPlayers() {
-        return players;
-    }
-
-    public Card getTopDiscardCard() {
-        return deck.topDiscardCard();
-    }
-
-    public void playerDrawCard(Player player) {
-        Card card = deck.drawCard();
-        if (card != null) {
-            player.addCardToHand(card);
-        } else {
-            System.out.println("El mazo de robar está vacío, reorganizando el mazo de descarte.");
-            deck.refillDrawPileFromDiscardPile();
-            card = deck.drawCard();
-            if (card != null) {
-                player.addCardToHand(card);
-            } else {
-                System.out.println("No fue posible robar una carta.");
-            }
+            return false;
         }
     }
 
 }
 
- */
--- src/main/java/co/icesi/edu/ui/Main.java
+++ src/main/java/co/icesi/edu/ui/Main.java
@@ -51,60 +51,72 @@ public class Main {
 
         // Bucle principal del juego
         while (!gameController.isGameOver()) {
-            System.out.println("\nEs el turno de " +  gameController.currentPlayer());
-            System.out.println("La carta en la cima del montón de descarte es: " + gameController.currentCard());
+            System.out.println("\n--------------------------------------------------");
+            System.out.println("Es el turno de " +  gameController.currentPlayer());
 
             int action = -1;
 
             boolean playedSuccessfully = false;
 
-            do {
-                System.out.println("¿Qué acción te gustaría realizar?");
-                System.out.println("1. Jugar una carta");
-                System.out.println("2. Robar del mazo");
-                System.out.print("   >");
-                action = scanner.nextInt();
-                scanner.nextLine();
-                while (action < 1 || action > 2) {
-                    System.out.println("Número inválido. Por favor, ingrese un número entre 1 y 2.");
+            if (gameController.isActivespecialcard()) {
+                gameController.handleSpecialCardEffect();
+                System.out.println("CHAN CHAN CHANNN");
+
+            } else {
+
+                do {
+                    System.out.println("La carta en la cima del montón de descarte es: " + gameController.currentCard());
+                    System.out.println("Tus cartas son: \n");
+                    System.out.println(gameController.currentPlayerCardList());
+
+                    System.out.println("¿Qué acción te gustaría realizar?");
+                    System.out.println("1. Jugar una carta");
+                    System.out.println("2. Robar del mazo");
                     System.out.print("   >");
                     action = scanner.nextInt();
                     scanner.nextLine();
-                }
-
-                if (action == 1) {
-                    System.out.print("Elige la carta que quieres jugar");
-                    System.out.println("Tus cartas son: \n");
-                    System.out.println(gameController.currentPlayerCardList());
-                    System.out.print("   >");
-                    int cardIndex = scanner.nextInt() - 1;
-                    scanner.nextLine();
-
-                    while (cardIndex < 0 || cardIndex >= gameController.handSizePlayer()) {
-                        System.out.println("Índice de carta inválido. Intentalo de nuevo");
+                    while (action < 1 || action > 2) {
+                        System.out.println("Número inválido. Por favor, ingrese un número entre 1 y 2.");
                         System.out.print("   >");
-                        cardIndex = scanner.nextInt() - 1;
+                        action = scanner.nextInt();
                         scanner.nextLine();
                     }
 
-                    playedSuccessfully = gameController.playCard(cardIndex);
-                    System.out.println(playedSuccessfully);
+                    if (action == 1) {
+                        System.out.print("Elige la carta que quieres jugar");
+                        System.out.print("   >");
+                        int cardIndex = scanner.nextInt() - 1;
+                        scanner.nextLine();
 
-                    if (!playedSuccessfully) {
-                        System.out.println("No puedes jugar esa carta, elige otra (1) o roba una del mazo (2).");
+                        while (cardIndex < 0 || cardIndex >= gameController.handSizePlayer()) {
+                            System.out.println("Índice de carta inválido. Intentalo de nuevo");
+                            System.out.print("   >");
+                            cardIndex = scanner.nextInt() - 1;
+                            scanner.nextLine();
+                        }
+
+                        playedSuccessfully = gameController.playCard(cardIndex);
+
+                        if (!playedSuccessfully) {
+                            System.out.println("No puedes jugar esa carta, elige otra (1) o roba una del mazo (2).");
+                        } else {
+                            gameController.nextTurn();
+                        }
+
+                        if (gameController.isGameOver()) {
+                            System.out.println("El juego ha terminado. ¡Felicidades " + gameController.currentPlayer() + ", has ganado!");
+                            break;
+                        }
+
+                    } else {
+                        gameController.drawCard(1);
+                        playedSuccessfully = true;
+                        gameController.nextTurn();
                     }
 
-                    if (gameController.isGameOver()) {
-                        System.out.println("El juego ha terminado. ¡Felicidades " + gameController.currentPlayer() + ", has ganado!");
-                        break;
-                    }
+                } while (!playedSuccessfully);
 
-                } else {
-                    gameController.drawCard(1);
-                    playedSuccessfully = true;
-                }
-
-            } while (!playedSuccessfully);
+            }
 
 
 
